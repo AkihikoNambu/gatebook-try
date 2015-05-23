5e838aa1275ae915b23987c67a7763786c364bbc
Akihiko Nambu <mate0rich.nmb@gmail.com>
2015-05-23 15:18:06 +0900

    commit

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -27,7 +27,7 @@ class NotesController < ApplicationController
     @note.title = params[:title]
     @note.content = params[:content]
     @note.save
-    redirect_to show_note_path(@note)
+    redirect_to show_note_path(@note.id)
   end
 
   def destroy
